7f90fc5e262eb8c7b0545c984518a584737b5af1
ayugithasari18-crypto <ayugithasari18@gmail.com>
2025-08-08 12:38:09 +0700

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,20 @@
+const GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbzT6goIff8W6_gkU_LImYwJJ2ztnpHpPBaMv3oTjjrq24PPHpbZJj0_o_FA2zfKaHpx/exec";
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetch(GAS_ENDPOINT)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("bank-name").textContent = data.bank;
+      document.getElementById("account-name").textContent = data.name;
+      document.getElementById("account-number").textContent = data.account;
+    });
+
+  document.getElementById("copy-btn").addEventListener("click", () => {
+    const accountNumber = document.getElementById("account-number").textContent;
+    navigator.clipboard.writeText(accountNumber).then(() => {
+      const note = document.getElementById("copy-notification");
+      note.classList.remove("hidden");
+      setTimeout(() => note.classList.add("hidden"), 3000);
+    });
+  });
+});
